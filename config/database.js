5b5a576cbe842130fd90d4cc3e197fb1f7cbcff4
David Wosk <dwosk@asperasoft.com>
2017-09-15 16:37:35 -0700

    Use cloud db in production environment

--- config/database.js
+++ config/database.js
@@ -1,4 +1,11 @@
 // config/database.js
+var url = 'mongodb://localhost/pixio'
+
+switch(process.env.NODE_ENV) {
+    case 'production':
+    url = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@ds135364.mlab.com:35364/pixio`
+};
+
 module.exports = {
-    'url' : 'mongodb://localhost/pixio'
+    'url' : url
 };
