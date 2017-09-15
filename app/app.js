710aa991a63e4b8f69e950c450d95eff88bd7565
David Wosk <dwosk@asperasoft.com>
2017-09-15 15:09:07 -0700

    Passport and additional configuration

--- app/app.js
+++ app/app.js
@@ -1,14 +1,27 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express  = require('express');
+var app      = express();
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
 
 var routes = require('../routes/index');
 var users = require('../routes/users');
 
-var app = express();
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+
+// require('./config/passport')(passport); // pass passport for configuration
+
 
 // view engine setup
 app.set('views', path.join(__dirname, '../views'));
@@ -16,11 +29,18 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+
+
+// required for passport
+app.use(session({ secret: 'secretsecretsecretsecret' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 app.use('/', routes);
 app.use('/users', users);
