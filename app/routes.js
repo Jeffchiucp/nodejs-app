a3bcbc7272edf8a613c5e74f00e46aacc2a9be41
David Wosk <dwosk@asperasoft.com>
2017-09-15 23:43:42 -0700

    Redirect to /profile

--- app/routes.js
+++ app/routes.js
@@ -30,7 +30,7 @@ module.exports = function(app, passport) {
     app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }),
         function(req, res) {
         // Successful authentication, redirect home.
-        res.redirect('/');
+        res.redirect('/profile');
     });
 
 
